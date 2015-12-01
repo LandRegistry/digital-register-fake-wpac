86224c36e245bb2cfc435f823a0e7714d3c0cd19
RichardProsser <richard.prosser@digital.landregistry.gov.uk>
2015-12-01 10:35:17 +0000

    Very simple dummy service; manual unit test OK.

--- mock_service.py
+++ mock_service.py
@@ -1,9 +1,26 @@
-from flask import Flask, jsonify, Response, request  # type: ignore
-import json, datetime
+import logging
+from flask import Flask
+#from flask.ext.api import status
+
+HTTP_200_OK = 200       # Should be status.HTTP_200_OK
+
 
 app = Flask(__name__)
 
 
+LOGGER = logging.getLogger(__name__)
+
+
+@app.route('/wp', methods=['GET', 'POST'])
+def fake_wpac():
+    """
+    Pretend to be WPAC.
+    """
+
+    return "", HTTP_200_OK
+
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(host='0.0.0.0', port=5555, debug=True)
+
